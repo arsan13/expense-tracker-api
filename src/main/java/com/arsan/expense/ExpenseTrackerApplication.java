bc5bd219696464beca98d6ead7d422a77865fb6a
arsan13 <arsan.anbu97@gmail.com>
2021-09-02 09:41:16 +0530

    Added "Swagger-2" for API Documentation

--- src/main/java/com/arsan/expense/ExpenseTrackerApplication.java
+++ src/main/java/com/arsan/expense/ExpenseTrackerApplication.java
@@ -8,7 +8,10 @@ import org.springframework.context.annotation.Bean;
 
 import com.arsan.expense.filter.AuthFilter;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@EnableSwagger2
 public class ExpenseTrackerApplication {
 
 	public static void main(String[] args) {
