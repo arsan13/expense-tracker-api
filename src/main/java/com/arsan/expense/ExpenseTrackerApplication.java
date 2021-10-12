255d074ba27d1ae288f85ca6ddbed4d4f51eb33e
arsan13 <arsan.anbu97@gmail.com>
2021-10-12 21:37:56 +0530

    Resolved CORS Policy

--- src/main/java/com/arsan/expense/ExpenseTrackerApplication.java
+++ src/main/java/com/arsan/expense/ExpenseTrackerApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 import com.arsan.expense.filter.AuthFilter;
 
@@ -28,6 +31,19 @@ public class ExpenseTrackerApplication {
 		AuthFilter authFilter = new AuthFilter();
 		registrationBean.setFilter(authFilter);
 		registrationBean.addUrlPatterns("/api/categories/*");
+		return registrationBean;
+	}
+	
+	@Bean
+	public FilterRegistrationBean<CorsFilter> corsFilter() {
+		FilterRegistrationBean<CorsFilter> registrationBean = new FilterRegistrationBean<>();
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		config.addAllowedOrigin("*");
+		config.addAllowedHeader("*");
+		source.registerCorsConfiguration("/**", config);
+		registrationBean.setFilter(new CorsFilter(source));
+		registrationBean.setOrder(0);
 		return registrationBean;
 	}
 	
