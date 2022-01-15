bb6855c403791de6788075fff795a6d47e5f294d
arsan13 <arsan.anbu97@gmail.com>
2022-01-15 22:06:44 +0530

    Increased validity of the Token

--- src/main/java/com/arsan/expense/Constants.java
+++ src/main/java/com/arsan/expense/Constants.java
@@ -4,5 +4,5 @@ public class Constants {
 
 	public static final String API_SECRET_KEY = "expensetrackerkey";
 	
-	public static final long TOKEN_VALIDITY = 2 * 60 * 60 * 1000; 	// 2hrs
+	public static final long TOKEN_VALIDITY = 24 * 60 * 60 * 1000; 	// 24hrs
 }
