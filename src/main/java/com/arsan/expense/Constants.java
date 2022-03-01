7fbe6273a03ca79faaa8b1078c400e96e77c7265
arsan13 <arsan.anbu97@gmail.com>
2022-03-01 13:23:59 +0530

    Increased token validity to 1 week

--- src/main/java/com/arsan/expense/Constants.java
+++ src/main/java/com/arsan/expense/Constants.java
@@ -4,5 +4,5 @@ public class Constants {
 
 	public static final String API_SECRET_KEY = "expensetrackerkey";
 	
-	public static final long TOKEN_VALIDITY = 24 * 60 * 60 * 1000; 	// 24hrs
+	public static final long TOKEN_VALIDITY = 7 * 24 * 60 * 60 * 1000; 	// 7 days
 }
